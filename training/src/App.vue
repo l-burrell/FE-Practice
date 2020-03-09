206e06a667d58a6146755595c7b659606e4dee03
burrell <eewinterz@gmail.com>
2020-03-08 20:15:00 -0400

    vue project setup

---
+++ training/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Navigation />
+    <Home />
+    <Blog />
+  </div>
+</template>
+
+<script>
+import Home from "./components/Home";
+import Blog from "./components/Blog";
+import Navigation from './components/Navigation';
+
+export default {
+  name: "App",
+  components: {
+    Home,
+    Blog,
+    Navigation,
+
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ training/src/components/Navigation.vue
@@ -0,0 +1,9 @@
+<template>
+<nav>
+    <ul>
+        <li><a href="">D</a></li>
+        <li><a href="">D</a></li>
+        <li><a href="">D</a></li>
+    </ul>
+</nav>
+</template>
