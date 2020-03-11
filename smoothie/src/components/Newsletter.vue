80709bb22eda03a089821911ec8b9c14761a638a
burrell <eewinterz@gmail.com>
2020-03-10 22:23:15 -0400

    smoothie vue app - implementing the newsletter with reviews

--- smoothie/src/components/Newsletter.vue
+++ smoothie/src/components/Newsletter.vue
@@ -1,5 +1,41 @@
 <template>
     <section id="newsletter">
-        newsletter will be here
+        <section class="review">
+            <h3>Timmy Turner</h3>
+            <p>What an amazing experience! We had our custom smoothie made within 3 minutes, and the lines were moving out the door fast!</p>
+        </section>
+        <section class="review">
+            <h3>Danial Grims</h3>
+            <p>What an amazing experience! We had our custom smoothie made within 3 minutes, and the lines were moving out the door fast!</p>
+        </section>
+        <section class="review">
+            <h3>Misty Oak</h3>
+            <p>What an amazing experience! We had our custom smoothie made within 3 minutes, and the lines were moving out the door fast!</p>
+        </section>
+        <section class="review">
+            <h3>Mike Landing</h3>
+            <p>What an amazing experience! We had our custom smoothie made within 3 minutes, and the lines were moving out the door fast!</p>
+        </section>
+        <section class="review">
+            <h3>Linda Lordamen</h3>
+            <p>What an amazing experience! We had our custom smoothie made within 3 minutes, and the lines were moving out the door fast!</p>
+        </section>
+        <section class="review">
+            <h3>Henry George</h3>
+            <p>What an amazing experience! We had our custom smoothie made within 3 minutes, and the lines were moving out the door fast!</p>
+        </section>
     </section>
 </template>
+<style scoped>
+section#newsletter {
+    display: flex;
+}
+section.review {
+    background-color: rgb(240, 238, 214);
+    padding: 10px;
+    width: 200px;
+}
+section.review h3 {
+    text-align: center;
+}
+</style>
