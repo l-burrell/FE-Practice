142a84d5221419d671abeaa970b34ded8f90d7c7
burrell <eewinterz@gmail.com>
2020-03-10 22:34:32 -0400

    smoothie vue app - newsletter styling in progress

--- smoothie/src/components/Newsletter.vue
+++ smoothie/src/components/Newsletter.vue
@@ -1,41 +1,55 @@
 <template>
     <section id="newsletter">
-        <section class="review">
-            <h3>Timmy Turner</h3>
-            <p>What an amazing experience! We had our custom smoothie made within 3 minutes, and the lines were moving out the door fast!</p>
-        </section>
-        <section class="review">
-            <h3>Danial Grims</h3>
-            <p>What an amazing experience! We had our custom smoothie made within 3 minutes, and the lines were moving out the door fast!</p>
-        </section>
-        <section class="review">
-            <h3>Misty Oak</h3>
-            <p>What an amazing experience! We had our custom smoothie made within 3 minutes, and the lines were moving out the door fast!</p>
-        </section>
-        <section class="review">
-            <h3>Mike Landing</h3>
-            <p>What an amazing experience! We had our custom smoothie made within 3 minutes, and the lines were moving out the door fast!</p>
-        </section>
-        <section class="review">
-            <h3>Linda Lordamen</h3>
-            <p>What an amazing experience! We had our custom smoothie made within 3 minutes, and the lines were moving out the door fast!</p>
-        </section>
-        <section class="review">
-            <h3>Henry George</h3>
-            <p>What an amazing experience! We had our custom smoothie made within 3 minutes, and the lines were moving out the door fast!</p>
+        <section id="review-box">
+            <section class="review">
+                <h3>Timmy Turner</h3>
+                <p>What an amazing experience! We had our custom smoothie made within 3 minutes, and the lines were moving out the door fast!</p>
+            </section>
+            <section class="review">
+                <h3>Danial Grims</h3>
+                <p>What an amazing experience! We had our custom smoothie made within 3 minutes, and the lines were moving out the door fast!</p>
+            </section>
+            <section class="review">
+                <h3>Misty Oak</h3>
+                <p>What an amazing experience! We had our custom smoothie made within 3 minutes, and the lines were moving out the door fast!</p>
+            </section>
+            <section class="review">
+                <h3>Mike Landing</h3>
+                <p>What an amazing experience! We had our custom smoothie made within 3 minutes, and the lines were moving out the door fast!</p>
+            </section>
+            <section class="review">
+                <h3>Linda Lordamen</h3>
+                <p>What an amazing experience! We had our custom smoothie made within 3 minutes, and the lines were moving out the door fast!</p>
+            </section>
+            <section class="review">
+                <h3>Henry George</h3>
+                <p>What an amazing experience! We had our custom smoothie made within 3 minutes, and the lines were moving out the door fast!</p>
+            </section>
         </section>
     </section>
 </template>
+
 <style scoped>
 section#newsletter {
+    background-color: rgb(248, 230, 230);
+    color: black;
+}
+section#review-box {
     display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    max-width: 90%;
+    margin: auto;
+
 }
 section.review {
     background-color: rgb(240, 238, 214);
-    padding: 10px;
-    width: 200px;
+    padding: 20px;
+    width: 400px;
+    margin: 20px;
 }
 section.review h3 {
     text-align: center;
+    padding: 10px;
 }
 </style>
