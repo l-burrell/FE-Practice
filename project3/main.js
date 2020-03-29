9313abb48b76260b73eb2d38a44ec9c4e7223ec8
burrell <eewinterz@gmail.com>
2020-03-28 23:09:05 -0400

    update f/c game

---
+++ project3/main.js
@@ -0,0 +1,8 @@
+
+var redBtn = document.getElementById("btnHot");
+var blueBtn = document.getElementById("btnCold");
+
+function beginGame() {
+    document.getElementById("welcome").style.display = "none";
+    document.getElementById("game").style.display = "block";
+}
