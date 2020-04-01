0a97ef914c535ea20756534cb6ceb04cfdfba83a
burrell <eewinterz@gmail.com>
2020-03-31 21:26:56 -0400

    mini math calc project - setup buttons

--- project4/main.js
+++ project4/main.js
@@ -3,3 +3,22 @@
 // DEAR DEVELOPERS, THIS CODE IS NOT FOR REUSE OR REDISTRIBUTION
 // THANKS FOR VIEWING
 
+//digits
+var btn0 = document.getElementById("btn0");
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var btn3 = document.getElementById("btn3");
+var btn4 = document.getElementById("btn4");
+var btn5 = document.getElementById("btn5");
+var btn6 = document.getElementById("btn6");
+var btn7 = document.getElementById("btn7");
+var btn8 = document.getElementById("btn8");
+var btn9 = document.getElementById("btn9");
+
+//controls
+var btnDecimal = document.getElementById("btnDecimal");
+var btnPlus = document.getElementById("btnPlus");
+var btnMinus = document.getElementById("btnMinus");
+var btnMultiply = document.getElementById("btnMultiply");
+var btnDivide = document.getElementById("btnDivide");
+var btnEnter = document.getElementById("btnEnter");
