fd3b9b916787ba6c5f4701e728d1cc237e174375
burrell <eewinterz@gmail.com>
2020-04-04 21:50:20 -0400

    update

--- project5/main.js
+++ project5/main.js
@@ -5,3 +5,6 @@ var cardTitle = document.getElementById('card-title');
 var cardDes = document.getElementById('card-description');
 var cardImg = document.getElementById("img");
 
+function turn() {
+    console.log("do something...")
+}
